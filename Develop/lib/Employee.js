92f0f244cbbccffb7ff5207ddd24f592d39b9529
S Galarza <sgalarza4190@gmail.com>
2020-11-23 16:36:53 -0500

    created initial files

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,15 @@
+// TODO: Write code to define and export the Employee class
+
+
+
+The first class is an Employee parent class with the following properties and methods:
+
+name
+id
+email
+getName()
+getId()
+getEmail()
+getRole() // Returns 'Employee'
+The other three classes will extend Employee.
+
